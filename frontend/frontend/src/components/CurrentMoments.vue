61665377eddf65c39bf2337f433f67a4c0c2e38c
Jie Lu <jie.lu@tuni.fi>
2025-03-28 21:41:05 +0200

    Add: frontend raw emoji display

--- frontend/frontend/src/components/CurrentMoments.vue
+++ frontend/frontend/src/components/CurrentMoments.vue
@@ -5,28 +5,32 @@ import { io, Socket} from 'socket.io-client';
 //define a type for moments
 interface Moments {
   timestamp: string;
-  emoji: string;
+  emote: string;
 }
 
 const moments = ref<Moments[]>([]);
 let socket: Socket;
 
 onMounted(() => {
-  // Use environment variable for WebSocket URL,    // socket = io('ws://server_a:3000');
-  socket = io(import.meta.env.VITE_SOCKET_URL );
-
+  // Use environment variable for WebSocket URL
+  socket = io('ws://localhost:3000' );
   // if we are using http, we write it like this
   //socket = io('http://localhost:3000' );
+   // socket = io(import.meta.env.VITE_SOCKET_URL  );
 
-  // Handle WebSocket errors
+  // const SOCKET_URL = import.meta.env.DEV
+  //   ? 'http://localhost:3000'    // Local development
+  //   : import.meta.env.VITE_SOCKET_URL || 'http://server-a:3000';  // Docker environment
+  //
+  // const socket = io(SOCKET_URL);
+
   socket.on('connect_error', (error) => {
     console.error('WebSocket connection failed:', error);
   });
 
-
   // Listen for incoming moments
     socket.on('current-moment', (moment: any) => {
-      if (!moment || !moment.timestamp || !moment.emoji) {
+      if (!moment || !moment.timestamp || !moment.emote) {
         console.warn("Invalid data received:", moment);
         return;
       }
@@ -36,7 +40,7 @@ onMounted(() => {
       // Add new moment at the start of the list
       moments.value.unshift({
         timestamp: moment.timestamp,
-        emoji: moment.emoji
+        emote: moment.emote
       });
 
       // Keep only the last 10 moments
@@ -65,7 +69,7 @@ onUnmounted(() => {
     <div v-for="(moment, index) in moments" :key="index"
         class="flex items-center mb--2 p-2 bg-gray-100 rounded">
       <span class="mr-2">{{moment.timestamp}}}</span>
-      <span class="text-2xl">{{moment.emoji}}}</span>
+      <span class="text-2xl">{{moment.emote}}}</span>
     </div>
   </div>
 </template>
