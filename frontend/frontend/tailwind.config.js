a4c7b98bab4e84feb6c89ecf69cfc55ab3bd91d0
Jie Lu <jie.lu@tuni.fi>
2025-04-05 00:22:04 +0300

    Fix: localhost and server b config

--- frontend/frontend/tailwind.config.js
+++ frontend/frontend/tailwind.config.js
@@ -1,10 +1,24 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{vue,js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  // purge: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
