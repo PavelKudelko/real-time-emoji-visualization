4265f4cbfdea571b40509980c5652fae46743eca
Pavel Kudelko <pavel.kudelko@tuni.fi>
2025-04-04 17:29:02 +0300

    Initial commit:server-b initial setup

---
+++ backend/server_b/index.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+// logging library for debug
+const morgan = require('morgan');
+const settingsRoutes = require('./routes/settings');
+
+const PORT = 3001;
+
+const http = require('http');
+const app = express();
+
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+const runServer = async() => {
+  server.listen(PORT, () => {
+    console.log(`server-b listening on port ${PORT}`);
+  });
+};
+
+runServer();
+
+app.use('/settings', settingsRoutes);
+
+// Catch all route for undefined routes
+app.use((req, res) => {
+  res.status(404).json({status: 404, message: 'Route not found'});
+});
