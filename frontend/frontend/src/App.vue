6640221477d075c20f2f6364bfb034c77c8b5bea
Jie Lu <jie.lu@tuni.fi>
2025-04-25 14:50:38 +0300

    Add: youtube never gonna give you up

--- frontend/frontend/src/App.vue
+++ frontend/frontend/src/App.vue
@@ -2,62 +2,51 @@
   <div class="justify-center ">
     <!--header area-->
     <div id="Mainheader" class="min-h-60">
-      <div id="header-bar" class="font-italic">imagine i have a menu bar</div>
-      <div id="search-bar">imagine i have a search bar</div>
+      <MenuBar/>
     </div>
 
     <!-- Main Content Area-->
-      <div id = "playwrap" class="relative flex  w-full ">
-          <!-- Placeholder for streaming video-->
-            <video-player class="relative flex-grow flex w-full h-full items-center justify-center" :options="videoOptions">
-              <div>
-                <!-- Left Side Current Emoji List-->
-                <CurrentMoments id="current-moment" class="absolute border-r-2 overflow-y-auto " />
-              </div>
-            </video-player>
-
+      <div id = "playwrap" class=" relative flex  w-full ">
+          <!-- Youtube video with Overlay-->
+        <div class=" relative flex-grow flex w-full items-center justify-center">
+        <YouTube
+          height="540"
+          width="960"
+          src="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          ref="youtube"/>
+          <div>
+            <!-- Left Side Current Emoji List-->
+            <CurrentMoments id="current-moment" class="absolute border-r-2 overflow-y-auto " />
+          </div>
+        </div>
       </div>
 
       <!-- Significant Moments bar  -->
-      <SignificantMoments/>
-      <ThresholdControl @update-threshold="handleThresholdUpdate"/>
+      <div>
+        <SignificantMoments/>
+        <ThresholdControl @update-threshold="handleThresholdUpdate"/>
+      </div>
   </div>
 
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { VideoPlayer } from '@videojs-player/vue';
+import YouTube from 'vue3-youtube';
 import 'video.js/dist/video-js.css';
 
 import './input.css'
 import CurrentMoments from './components/CurrentMomentsDisplay.vue';
 import SignificantMoments from './components/SignificantMomentsDisplay.vue';
 import ThresholdControl from "@/components/EmojiDisplayControls.vue";
+import MenuBar from "@/components/MenuBar.vue";
 
-
-
-const videoOptions = ref({
-  sources: [{
-    src: 'placeholder.mp4',
-    type: 'video/mp4'
-    // kinda wanna put her never gonna give you up never gonna let you down
-  }],
-  controls: true,
-  aspectRatio: "16:9",
-
-});
 
 const threshold = ref(50);
 
 const handleThresholdUpdate = (newThreshold:number) => {
   threshold.value = newThreshold;
 };
-
-// onMounted(()=>{
-//   emojiBlasts({emojis:["💖","❤️", "🧡", "💛", "💚", "💙", "💜"],
-//   });
-// });
 
 </script>
 
